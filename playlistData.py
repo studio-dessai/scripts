4b0104a5cc163f1b38bd307e13595fd7ac4ba6f8
Lochlan Morrissey <lm@loch.land>
2015-02-28 17:06:22 +1000

    Added testing file, split file into tracklist. Need to write group capturing regex to worth through file syntax.

---
+++ playlistData.py
@@ -0,0 +1,29 @@
+""" Script to extract data from studio d'essai playlists. """
+
+import re # for regex
+import os # for filesystem
+import csv
+
+for i in os.listdir():
+    if i[-4].isnumeric(): #  tests for if i is a playlist file by checking date in filename
+        f = open(i)
+        fi = f.read()
+
+        tracks = fi.splitlines()
+        for i in tracks:
+            if str(i).__contains__('Playlist for'): tracks.remove(i)
+
+        # defines date variable; allowable because only one date per file
+        re_date = re.compile('\d{4}\.\d{2}\.\d{2}')
+        date = re_date.search(fi).group()
+
+        for track in tracks:
+            if len(track) < 1:
+                continue
+            else:
+
+
+
+print(tracks)
+
+#         print(re_artist.search(track).group())
