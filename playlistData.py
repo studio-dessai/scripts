ab38399ef345966aed3255f2a71ae2e3073276a8
Lochlan Morrissey <lm@loch.land>
2015-03-07 11:45:13 +1000

    Exception handling added. Outputs warning for manual entry.

--- playlistData.py
+++ playlistData.py
@@ -25,39 +25,47 @@ for i in argv[1:]:
         if len(track) < 1:
             continue
         else:
+            try:
 #               artist finder
-            re_artist = re.compile('1.\s\[?([\w\s\d]+)[\]\w]')
-            artist = re_artist.search(track).group(1)
+                re_artist = re.compile('1.\s\[?([\w\s\d]+)[\]\w]')
+                artist = re_artist.search(track).group(1)
 
 #               brainz link finder
-            re_brainz = re.compile(r'(\]\((\S+musicbrainz.org\/\S+)\))')
-            if re_brainz.search(track) is None: brainz = ""
-            else: brainz = re_brainz.search(track).group(2)
+                re_brainz = re.compile(r'(\]\((\S+musicbrainz.org\/\S+)\))')
+                if re_brainz.search(track) is None: brainz = ""
+                else: brainz = re_brainz.search(track).group(2)
 
 #               featured artist
-            re_featArtist = re.compile('feat\.\s\[?([\w\s\d]+)\](\((\S+musicbrainz.org\S+)\))?')
-            if re_featArtist.search(track) is None:
-                featArtist = ""
-                featArtistBrainz = ""
-            else:
-                featArtist = re_featArtist.search(track).group(1)
-                featArtistBrainz = re_featArtist.search(track).group(3)
+                re_featArtist = re.compile('feat\.\s\[?([\w\s\d]+)\](\((\S+musicbrainz.org\S+)\))?')
+                if re_featArtist.search(track) is None:
+                    featArtist = ""
+                    featArtistBrainz = ""
+                else:
+                    featArtist = re_featArtist.search(track).group(1)
+                    featArtistBrainz = re_featArtist.search(track).group(3)
 
 #               track title
-            re_title = re.compile('\s[-]\s(.+)\s\(')
-            title = re_title.search(track).group(1)
+                re_title = re.compile('\s[-]\s([\s\w\d]+)\s\(')
+                title = re_title.search(track).group(1)
 
 #               the remainder (best to do with one regex, as it's super regular)
-            re_otherInf = re.compile('\(_(.+)_,\s+(.+),\s+(.+),\s+(.+)\)')
-            if re_otherInf.search(track) is None:
-                continue
-            album = re_otherInf.search(track).group(1)
-            label = re_otherInf.search(track).group(2)
-            year = re_otherInf.search(track).group(3)
-            country = re_otherInf.search(track).group(4)
+                re_otherInf = re.compile('\(_(.+)_,\s+(.+),\s+(.+),\s+(.+)\)')
+                if re_otherInf.search(track) is None:
+                    continue
+                album = re_otherInf.search(track).group(1)
+                label = re_otherInf.search(track).group(2)
+                year = re_otherInf.search(track).group(3)
+                country = re_otherInf.search(track).group(4)
 
-            with open('dat.csv', 'a', newline='\n') as output:
-                # note! opening dat.csv with 'a' allows amending lines instead of writing over them.
-                datwriter = csv.writer(output, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-                datwriter.writerow([date, artist, brainz, title, album, label, year, country])
+                with open('dat.csv', 'a', newline='\n') as output:
+                    # note! opening dat.csv with 'a' allows amending lines instead of writing over them.
+                    datwriter = csv.writer(output, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+                    datwriter.writerow([date, artist, brainz, title, album, label, year, country])
+            except:
+                with open('dat.csv', 'a', newline='\n') as output:
+                    datwriter = csv.writer(output, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+                    datwriter.writerow(['irregular: needs manual input'])
+                continue
+
+
     f.close()
