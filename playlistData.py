7ab1a512b77880bd357ba85074e349d17537074c
Lochlan Morrissey <lm@loch.land>
2015-03-07 10:13:26 +1000

    Accepts args; unindented most of file.

--- playlistData.py
+++ playlistData.py
@@ -7,49 +7,49 @@ At the moment, I have 'print' commands as feedback. These will eventually be con
 import re # for regex
 import os # for filesystem
 import csv
+from sys import argv
 
-for i in os.listdir():
-    if i[-4].isnumeric(): #  tests for if i is a playlist file by checking date in filename
-        f = open(i)
-        fi = f.read()
+for i in argv[1:]:
+    f = open(i)
+    fi = f.read()
 
-        tracks = fi.splitlines()
-        for i in tracks:
-            if str(i).__contains__('Playlist for'): tracks.remove(i)
+    tracks = fi.splitlines()
+    for i in tracks:
+        if str(i).__contains__('Playlist for'): tracks.remove(i)
 
-        # defines date variable; allowable because only one date per file
-        re_date = re.compile('\d{4}\.\d{2}\.\d{2}')
-        date = re_date.search(fi).group()
+    # defines date variable; allowable because only one date per file
+    re_date = re.compile('\d{4}\.\d{2}\.\d{2}')
+    date = re_date.search(fi).group()
 
-        for track in tracks:
-            if len(track) < 1:
-                continue
-            else:
+    for track in tracks:
+        if len(track) < 1:
+            continue
+        else:
 #               artist finder
-                re_artist = re.compile('1.\s\[?([\w\s\d]+)[\]\w]')
-                artist = re_artist.search(track).group(1)
+            re_artist = re.compile('1.\s\[?([\w\s\d]+)[\]\w]')
+            artist = re_artist.search(track).group(1)
 
 #               brainz link finder
-                re_brainz = re.compile(r'(\]\((\S+musicbrainz.org/\S+)\))')
-                brainz = re_brainz.search(track).group(2)
+            re_brainz = re.compile(r'(\]\((\S+musicbrainz.org/\S+)\))')
+            brainz = re_brainz.search(track).group(2)
 
 #               featured artist
-                re_featArtist = re.compile('feat\.\s\[?([\w\s\d]+)\](\((\S+musicbrainz.org\S+)\))?')
-                if re_featArtist.search(track) is None:
-                    featArtist = ""
-                    featArtistBrainz = ""
-                else:
-                    featArtist = re_featArtist.search(track).group(1)
-                    featArtistBrainz = re_featArtist.search(track).group(3)
+            re_featArtist = re.compile('feat\.\s\[?([\w\s\d]+)\](\((\S+musicbrainz.org\S+)\))?')
+            if re_featArtist.search(track) is None:
+                featArtist = ""
+                featArtistBrainz = ""
+            else:
+                featArtist = re_featArtist.search(track).group(1)
+                featArtistBrainz = re_featArtist.search(track).group(3)
 
 #               track title
-                re_title = re.compile('\s[-]\s([\w\s]+)\s\(')
-                title = re_title.search(track).group(1)
+            re_title = re.compile('\s[-]\s([\w\s]+)\s\(')
+            title = re_title.search(track).group(1)
 
 #               the remainder (best to do with one regex, as it's super regular)
-                re_otherInf = re.compile('\(_(.+)_,\s+(.+),\s+(.+),\s+(.+)\)')
-                album = re_otherInf.search(track).group(1)
-                label = re_otherInf.search(track).group(2)
-                year = re_otherInf.search(track).group(3)
-                country = re_otherInf.search(track).group(4)
-                print("1. [{0}]({1}) - {2} (_{3}_, {4}, {5}, {6})".format(artist, brainz, title, album, label, year, country))
+            re_otherInf = re.compile('\(_(.+)_,\s+(.+),\s+(.+),\s+(.+)\)')
+            album = re_otherInf.search(track).group(1)
+            label = re_otherInf.search(track).group(2)
+            year = re_otherInf.search(track).group(3)
+            country = re_otherInf.search(track).group(4)
+            print("1. [{0}]({1}) - {2} (_{3}_, {4}, {5}, {6})".format(artist, brainz, title, album, label, year, country))
